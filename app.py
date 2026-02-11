847b883abb705eafd1cdbc43a6978c1b12866634
sophosnotificationtest-max <sophosnotificationtest@gmail.com>
2026-02-11 17:26:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
