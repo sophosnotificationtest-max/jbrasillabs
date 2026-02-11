80226a847f79e5e6ede44fccfe3717f423fc74ab
sophosnotificationtest-max <sophosnotificationtest@gmail.com>
2026-02-11 17:33:39 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # importa direto a instância Flask
+from app import app  # importa o objeto Flask direto
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
